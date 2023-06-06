c1a8822fc1ffce3a22bfc0cc58e97916c85ef72e
CELYNEBIRUNGI <113887926+CELYNEBIRUNGI@users.noreply.github.com>
2023-06-06 13:16:25 +0300

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
 import displayList from './modules/display.js';
-import { addList, removeList, editList } from './modules/operation.js';
-import './index.css';
 
+import { addList, removeList, editList } from './modules/operation.js';
+
+// import './index.css';
 const allList = JSON.parse(localStorage.getItem('todo')) || [];
+
 const bkList = document.querySelector('.lists');
 displayList(bkList, allList);
 const edit = document.querySelector('.editTask');
 const arrow = document.querySelector('.arrow');
 arrow.addEventListener('click', () => {
-  addList(allList, edit.value);
-  window.location.reload();
+  if (edit.value !== '') {
+    addList(allList, edit.value);
+    const newList = JSON.parse(localStorage.getItem('todo')) || [];
+    bkList.textContent = '';
+    displayList(bkList, newList);
+    edit.value = '';
+  }
 });
-const dots = document.querySelectorAll('.dots');
-dots.forEach((dot) => {
-  dot.addEventListener('click', () => {
-    removeList(dot.parentNode.className, allList);
-    window.location.reload();
+bkList.addEventListener('click', () => {
+  const dots = document.querySelectorAll('.dots');
+  dots.forEach((dot) => {
+    dot.addEventListener('click', () => {
+      removeList(dot.parentNode.className, allList);
+      const newList = JSON.parse(localStorage.getItem('todo')) || [];
+      bkList.textContent = '';
+      displayList(bkList, newList);
+    });
   });
-});
-const description = document.querySelectorAll('.list-desc');
-description.forEach((desc) => {
-  desc.addEventListener('input', () => {
-    const list = desc.parentNode.className;
-    const newDesc = desc.textContent;
-    editList(list - 1, newDesc, allList);
+
+  const description = document.querySelectorAll('.list-desc');
+  description.forEach((desc) => {
+    desc.addEventListener('input', () => {
+      const list = desc.parentNode.className;
+      const newDesc = desc.textContent;
+      editList(list - 1, newDesc, allList);
+    });
   });
 });
--- src/modules/display.js
+++ src/modules/display.js
@@ -28,4 +28,3 @@ const displayList = (lists, allList) => {
     });
   };
   export default displayList;
-  
