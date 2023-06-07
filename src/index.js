1f7da7d322552df390f146e5105fcd7086fc9be0
CELYNEBIRUNGI <113887926+CELYNEBIRUNGI@users.noreply.github.com>
2023-06-07 11:24:42 +0300

    modify index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import { addList, removeList, editList } from './modules/operation.js';
 import { updateStatus, clearCompleted } from './modules/checkout.js';
-import './index.css'
+import './index.css';
 
 const allList = JSON.parse(localStorage.getItem('todo')) || [];
 const bkList = document.querySelector('.lists');
-displayList(bkList, allList);
+
+const createListItem = (list) => {
+  const listItem = document.createElement('li');
+  const itemDiv = document.createElement('div');
+  const checkbox = document.createElement('input');
+  const label = document.createElement('label');
+  const deleteIcon = document.createElement('i');
+
+  itemDiv.className = `${list.index}`;
+  label.className = 'list-desc';
+  listItem.className = `items ${list.index}`;
+  checkbox.type = 'checkbox';
+  checkbox.id = `checkbox-${list.index}`;
+  checkbox.className = 'checkbox';
+  checkbox.name = `checkbox-${list.index}`;
+  checkbox.checked = list.completed;
+  label.textContent = list.description;
+  label.setAttribute('contenteditable', 'true');
+  deleteIcon.className = 'fa fa-trash delete';
+  itemDiv.appendChild(checkbox);
+  itemDiv.appendChild(label);
+  listItem.appendChild(itemDiv);
+  listItem.appendChild(deleteIcon);
+
+  return listItem;
+};
 
 const edit = document.querySelector('.editTask');
 const arrow = document.querySelector('.arrow');
@@ -12,7 +37,11 @@ arrow.addEventListener('click', () => {
   if (edit.value !== '') {
     addList(allList, edit.value);
     edit.value = '';
-    displayList(bkList, allList);
+    bkList.textContent = '';
+    allList.forEach((list) => {
+      const listItem = createListItem(list);
+      bkList.appendChild(listItem);
+    });
   }
 });
 
@@ -55,5 +84,9 @@ bkList.addEventListener('change', (event) => {
 const clearBtn = document.querySelector('.btn-clear');
 clearBtn.addEventListener('click', () => {
   const updatedList = clearCompleted(allList);
-  displayList(bkList, updatedList);
+  bkList.textContent = '';
+  updatedList.forEach((list) => {
+    const listItem = createListItem(list);
+    bkList.appendChild(listItem);
+  });
 });
